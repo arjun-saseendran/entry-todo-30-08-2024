0a72661eedfef50d2c64bc8a9564c36b2a315592
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-30 12:56:49 +0530

    add main section

--- src/components/main/MainSection.jsx
+++ src/components/main/MainSection.jsx
@@ -1,8 +1,10 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import  Col  from 'react-bootstrap/Col';
 
 function MainSection() {
   return (
+    <Col-12 sm={6} md={4} lg={3}>
     <Card >
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
@@ -14,6 +16,7 @@ function MainSection() {
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
+    </Col-12>
   );
 }
 
